d4929ade83df0dad8770e6f5d7b3aa3558b4b4b3
LamGitKraken <lamitptit99@gmail.com>
2021-04-08 17:05:25 +0700

    create database in mysql from model

--- onlineshop/settings.py
+++ onlineshop/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'onlineshop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djangodatabase',
+        'USER': 'dbadmin',
+        'PASSWORD': 'lam@proptit99',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
